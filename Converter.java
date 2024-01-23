8f2dbf53a52f50dff9a82c149353078bbbf3865a
Your Name <fdfdfss@fedsf.com>
2024-01-23 23:54:01 +0200

    add main

---
+++ Converter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Converter {
+    public static void main(String[] args) {
+        Scanner conversion = new Scanner(System.in);
+        double mil = 1.6;
+        System.out.println("Введите значение:");
+        double sum = conversion.nextDouble();
+        double converted = mil*sum;
+        System.out.println("Конвертація кілометрів в милі "+converted);
+    }
+}
